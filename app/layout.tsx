92644c57f61cca25c261bc439810e63487459105
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-13 08:19:32 +0800

    feat(layout): add mobile view handler

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Outfit } from "next/font/google"
 import "./globals.css"
 import Navbar from "@/components/navbar"
+import { Separator } from "@/components/ui/separator"
 
 const outfit = Outfit({ subsets: ["latin"], variable: "--font-Outfit", display: "swap" })
 
@@ -13,11 +14,21 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className={`${outfit.variable}`}>
-			<body className="flex w-full h-full">
-				<div className="w-[15%] grow ml-4">
-					<Navbar />
+			<body className="w-full h-full">
+				<div className="flex w-full h-full max-sm:hidden">
+					<div className="w-[15%] grow ml-4">
+						<Navbar />
+					</div>
+					<div className="w-full h-real grow m-4">{children}</div>
 				</div>
-				<div className="w-full h-real grow m-4">{children}</div>
+				<div className="h-screen flex flex-col justify-center place-items-center space-y-4">
+					<div className="flex place-items-center space-x-4 sm:hidden">
+						<div className="text-2xl font-medium tracking-wide">418</div>
+						<Separator orientation="vertical" className="h-10 bg-neutral-500" />
+						<div className="tracking-wide">I'm a teapot.</div>
+					</div>
+					<div className="text-xs">Please use a tablet or laptop to view this page.</div>
+				</div>
 			</body>
 		</html>
 	)
