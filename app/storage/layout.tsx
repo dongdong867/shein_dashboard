9761f40b60e487ed2245676ead7d8795c59ef9bd
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-13 13:19:27 +0800

    feat(storage-layout): remove temp bg of factory orders

--- app/storage/layout.tsx
+++ app/storage/layout.tsx
@@ -1,11 +1,10 @@
 import { FactoryOrders } from "@/components/factory-orders"
 
-
 const StorageLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="flex w-full h-full">
 			<div className="w-full h-real grow">{children}</div>
-			<div className="bg-black grow ml-4">
+			<div className="grow ml-4">
 				<FactoryOrders />
 			</div>
 		</div>
