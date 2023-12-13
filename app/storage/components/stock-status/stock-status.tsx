87a22d7a5e11670df171b3a89944e6f96d45d542
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-14 00:16:42 +0800

    style(stock-status): format code

--- app/storage/components/stock-status/stock-status.tsx
+++ app/storage/components/stock-status/stock-status.tsx
@@ -5,14 +5,14 @@ export const StockStatus = () => {
 	return (
 		<div className="w-full h-1/3 min-h-[320px] flex flex-col justify-start place-items-start rounded-[32px] bg-base/90 overflow-y-scroll">
 			<div className="w-full flex justify-between place-items-center px-7 pt-7">
-			<div className="-space-y-1 font-bold mb-4">
-				<div className="text-2xl">實時倉庫容量概況</div>
-				<div className="text-sm">資料時間: {" "} 3204/02/30 25:61</div>
+				<div className="-space-y-1 font-bold mb-4">
+					<div className="text-2xl">實時倉庫容量概況</div>
+					<div className="text-sm">資料時間: 3204/02/30 25:61</div>
 				</div>
 				<StockStatusReloadButton />
 			</div>
 			<div className="w-full h-full flex justify-start place-items-center space-x-4 overflow-y-scroll pl-7">
-			<StockStatusModal percentage={100} name="test" />
+				<StockStatusModal percentage={100} name="test" />
 			</div>
 		</div>
 	)
