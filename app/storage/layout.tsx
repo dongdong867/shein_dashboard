c2d4899464abd8eb4cd56e07083317c1d096aefc
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-13 11:44:43 +0800

    fix(storage-layout): rename orders to factory-orders

--- app/storage/layout.tsx
+++ app/storage/layout.tsx
@@ -1,11 +1,12 @@
-import Orders from "./components/orders"
+import { FactoryOrders } from "@/components/factory-orders"
+
 
 const StorageLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="flex w-full h-full">
 			<div className="w-full h-real grow">{children}</div>
 			<div className="bg-black grow ml-4">
-				<Orders />
+				<FactoryOrders />
 			</div>
 		</div>
 	)
