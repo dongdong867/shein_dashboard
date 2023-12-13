fc8810a8717b08163857a60a4426b1bf9b321668
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-13 11:47:25 +0800

    feat(storage): implement stock status to storage page

--- app/storage/page.tsx
+++ app/storage/page.tsx
@@ -2,7 +2,11 @@ import { StockStatus } from "./components/stock-status"
 
 const StoragePage = () => {
 	return (
-		<div className="h-full flex justify-center bg-neutral-500/90 rounded-[32px]">
+		<div className="w-full h-full p-10 flex flex-col justify-start bg-neutral-500/90 rounded-[32px] overflow-hidden">
+			<div className="text-primary-foreground font-bold mb-8 space-y-2">
+				<div className="text-sm">SHEIN 數位轉型計畫</div>
+				<div className="text-5xl">倉儲管理系統</div>
+			</div>
 			<StockStatus />
 		</div>
 	)
