8926930c6c8db822ffe61bcb06bc64c6077d6508
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-13 13:17:00 +0800

    fix(navbar): fix navbar not sticky error

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,15 +14,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className={`${outfit.variable}`}>
-			<body className="w-screen h-full overflow-hidden">
+			<body className="w-screen h-full overflow-x-hidden">
 				<div className="flex w-full h-full p-4 space-x-4 max-sm:hidden">
-					<div className="w-[15%] max-w-[180px] h-full">
-						<Navbar />
+					<div className="relative w-[15%] max-w-[180px] grow">
+						<div className="sticky top-4 w-full h-[calc(100vh-32px)] bg-neutral-500/90 rounded-[32px]">
+							<Navbar />
+						</div>
 					</div>
-					<div className="w-[80%] h-real grow">{children}</div>
+					<div className="w-[80%] grow">{children}</div>
 				</div>
-				<div className="h-screen flex flex-col justify-center place-items-center space-y-4">
-					<div className="flex place-items-center space-x-4 sm:hidden">
+				<div className="h-screen flex flex-col justify-center place-items-center space-y-4 sm:hidden">
+					<div className="flex place-items-center space-x-4">
 						<div className="text-2xl font-medium tracking-wide">418</div>
 						<Separator orientation="vertical" className="h-10 bg-neutral-500" />
 						<div className="tracking-wide">I'm a teapot.</div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,78 +9,74 @@ import SheinLogo from "/public/shein-logo.png"
 
 const Navbar = () => {
 	return (
-		<div className="sticky top-0 h-screen">
-			<div className="h-full pb-8">
-				<div className="w-full h-full p-2.5 flex flex-col place-items-center bg-neutral-500/90 rounded-[32px]">
-					{/* Logo */}
-					<div className="h-max flex justify-center place-items-center mx-auto pt-6 space-x-2">
-						<Image src={SheinLogo} alt="" className="w-10 h-10 aspect-square" />
-						<div className="text-white text-3xl xl:text-4xl font-bold max-lg:hidden">SHEIN</div>
-					</div>
-					<div className="h-16" />
+		<div className="w-full h-full p-2.5 flex flex-col place-items-center ">
+			{/* Logo */}
+			<div className="h-max flex justify-center place-items-center mx-auto pt-6 space-x-2">
+				<Image src={SheinLogo} alt="" className="w-10 h-10 aspect-square" />
+				<div className="text-white text-3xl xl:text-4xl font-bold max-lg:hidden">SHEIN</div>
+			</div>
+			<div className="h-16" />
 
-					{/* Link to Storage Management System */}
-					<div className="flex flex-col space-y-3 text-white">
-						<Link href={"/storage"}>
-							<Button variant={"ghost"} asChild className="h-max flex justify-between rounded-2xl">
-								<div>
-									<div className="flex place-items-center space-x-3 max-lg:hidden">
-										<FaBox size={20} />
-										<div className="text-lg xl:text-xl">庫存管理系統</div>
-									</div>
-									<div className="lg:hidden">
-										<FaBox size={30} />
-									</div>
-								</div>
-							</Button>
-						</Link>
+			{/* Link to Storage Management System */}
+			<div className="flex flex-col space-y-3 text-white">
+				<Link href={"/storage"}>
+					<Button variant={"ghost"} asChild className="h-max flex justify-between rounded-2xl">
+						<div>
+							<div className="flex place-items-center space-x-3 max-lg:hidden">
+								<FaBox size={20} />
+								<div className="text-lg xl:text-xl">庫存管理系統</div>
+							</div>
+							<div className="lg:hidden">
+								<FaBox size={30} />
+							</div>
+						</div>
+					</Button>
+				</Link>
 
-						{/* Link to Output Management System */}
-						<Link href={"/"}>
-							<Button variant={"ghost"} asChild className="h-max flex justify-between rounded-2xl">
-								<div>
-									<div className="w-full flex justify-between place-items-center space-x-3 max-lg:hidden">
-										<FaChartArea size={20} />
-										<div className="text-lg xl:text-xl">生產量管理</div>
-									</div>
-									<div className="lg:hidden">
-										<FaChartArea size={30} />
-									</div>
-								</div>
-							</Button>
-						</Link>
+				{/* Link to Output Management System */}
+				<Link href={"/"}>
+					<Button variant={"ghost"} asChild className="h-max flex justify-between rounded-2xl">
+						<div>
+							<div className="w-full flex justify-between place-items-center space-x-3 max-lg:hidden">
+								<FaChartArea size={20} />
+								<div className="text-lg xl:text-xl">生產量管理</div>
+							</div>
+							<div className="lg:hidden">
+								<FaChartArea size={30} />
+							</div>
+						</div>
+					</Button>
+				</Link>
 
-						{/* Link to Order Management System */}
-						<Link href={"/"}>
-							<Button variant={"ghost"} asChild className="h-max flex justify-between rounded-2xl">
-								<div>
-									<div className="flex place-items-center space-x-3 max-lg:hidden">
-										<FaClipboardList size={20} />
-										<div className="text-lg xl:text-xl">訂單管理系統</div>
-									</div>
-									<div className="lg:hidden">
-										<FaClipboardList size={30} />
-									</div>
-								</div>
-							</Button>
-						</Link>
+				{/* Link to Order Management System */}
+				<Link href={"/"}>
+					<Button variant={"ghost"} asChild className="h-max flex justify-between rounded-2xl">
+						<div>
+							<div className="flex place-items-center space-x-3 max-lg:hidden">
+								<FaClipboardList size={20} />
+								<div className="text-lg xl:text-xl">訂單管理系統</div>
+							</div>
+							<div className="lg:hidden">
+								<FaClipboardList size={30} />
+							</div>
+						</div>
+					</Button>
+				</Link>
 
-						{/* Link to Factor Processing System */}
-						<Link href={"/"}>
-							<Button variant={"ghost"} asChild className="h-max flex justify-between rounded-2xl">
-								<div>
-									<div className="flex place-items-center space-x-3 max-lg:hidden">
-										<MdFactory size={20} />
-										<div className="text-lg xl:text-xl">工廠排程系統</div>
-									</div>
-									<div className="lg:hidden">
-										<MdFactory size={30} />
-									</div>
-								</div>
-							</Button>
-						</Link>
-					</div>
-				</div>
+				{/* Link to Factor Processing System */}
+				<Link href={"/"}>
+					<Button variant={"ghost"} asChild className="h-max flex justify-between rounded-2xl">
+						<div>
+							<div className="flex place-items-center space-x-3 max-lg:hidden">
+								<MdFactory size={20} />
+								<div className="text-lg xl:text-xl">工廠排程系統</div>
+							</div>
+							<div className="lg:hidden">
+								<MdFactory size={30} />
+							</div>
+						</div>
+					</Button>
+				</Link>
 			</div>
 		</div>
 	)
