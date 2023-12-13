ade2d2d3f291fb838fa67c9a0c3eadf3038c43f8
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-13 13:25:03 +0800

    feat(storage): change spacing logic

--- app/storage/page.tsx
+++ app/storage/page.tsx
@@ -2,10 +2,10 @@ import { StockStatus } from "./components/stock-status"
 
 const StoragePage = () => {
 	return (
-		<div className="w-full h-full p-10 flex flex-col justify-start bg-neutral-500/90 rounded-[32px] overflow-hidden">
-			<div className="text-primary-foreground font-bold mb-8 space-y-2">
+		<div className="w-full h-full p-10 space-y-8 flex flex-col justify-start bg-neutral-500/90 rounded-[32px] overflow-hidden">
+			<div className="text-primary-foreground font-bold space-y-2">
 				<div className="text-sm">SHEIN 數位轉型計畫</div>
-				<div className="text-5xl">倉儲管理系統</div>
+				<div className="text-4xl">倉儲管理系統</div>
 			</div>
 			<StockStatus />
 		</div>
