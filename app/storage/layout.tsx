72bc8cd12dd9d674bd60e26d0398cf145c0b1c7e
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-14 15:50:11 +0800

    feat(storage-layout): make factory orders sticky to top

--- app/storage/layout.tsx
+++ app/storage/layout.tsx
@@ -2,9 +2,9 @@ import { FactoryOrders } from "@/components/factory-orders"
 
 const StorageLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="flex w-full h-full">
+		<div className="flex w-full">
 			<div className="w-full h-real grow">{children}</div>
-			<div className="grow ml-4">
+			<div className="sticky top-4 h-[calc(100vh-32px)] ml-4 max-[1400px]:hidden">
 				<FactoryOrders />
 			</div>
 		</div>
