3229703ace4e5756b11e48f116bfe9cecfc794c9
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-15 13:51:32 +0800

    fix(firestore): replace cert.json with env variable

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -1,13 +1,14 @@
-// file
-import certJson from "@/cert.json"
 // utils
 import { credential } from "firebase-admin"
 import { ServiceAccount, getApp, getApps, initializeApp } from "firebase-admin/app"
 import { getFirestore } from "firebase-admin/firestore"
 
-const app = !getApps().length
-	? initializeApp({ credential: credential.cert(certJson as ServiceAccount) })
-	: getApp()
+const app = !getApps().length ? initializeApp({
+	credential: credential.cert({
+		projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+		privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+		clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL
+} as ServiceAccount)}) : getApp()
 
 const adminFirestore = getFirestore(app)
 adminFirestore.settings({ ignoreUndefinedProperties: true })
