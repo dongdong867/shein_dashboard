5a0bbf104b322f2deb68a0a5cad0c828004f444c
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-17 14:56:25 +0800

    fix(firebase): add private key process

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase-admin/firestore"
 const app = !getApps().length ? initializeApp({
 	credential: credential.cert({
 		projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-		privateKey: process.env.FIREBASE_PRIVATE_KEY,
+		privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 		clientEmail: process.env.FIREBASE_CLIENT_EMAIL
 } as ServiceAccount)}) : getApp()
 
