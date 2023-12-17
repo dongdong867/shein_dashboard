d3e9fdfd7212d3ca698d10dc1bb70b40c7f570d7
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-17 14:54:45 +0800

    feat(firebase): update environment variable names

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -6,8 +6,8 @@ import { getFirestore } from "firebase-admin/firestore"
 const app = !getApps().length ? initializeApp({
 	credential: credential.cert({
 		projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-		privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
-		clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL
+		privateKey: process.env.FIREBASE_PRIVATE_KEY,
+		clientEmail: process.env.FIREBASE_CLIENT_EMAIL
 } as ServiceAccount)}) : getApp()
 
 const adminFirestore = getFirestore(app)
