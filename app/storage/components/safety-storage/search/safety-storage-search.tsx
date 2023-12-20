48c1892266a80f7d0d3054358fdb8617bbf8579c
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-20 15:26:17 +0800

    feat(safety-storage): create search bar

--- app/storage/components/safety-storage/search/safety-storage-search.tsx
+++ app/storage/components/safety-storage/search/safety-storage-search.tsx
@@ -1,7 +1,16 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { IoSearch } from "react-icons/io5"
+
 export const SafetyStorageSearch = () => {
 	return (
-		<div>
-			<div></div>
+		<div className="w-full flex justify-end place-items-center space-x-2">
+			<Input placeholder="輸入商品編號..." className="w-full xl:w-1/2 px-4" />
+			<Button variant={"default"} asChild className="w-[32.5px] h-[32.5px] p-1 cursor-pointer">
+				<IoSearch size={40} />
+			</Button>
 		</div>
 	)
 }
