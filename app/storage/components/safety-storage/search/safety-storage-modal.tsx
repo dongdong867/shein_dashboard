b323c921bfab5a82a8927a79814caadb23b065dd
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-22 00:24:23 +0800

    feat(safety-storage): create safety storage modal

--- app/storage/components/safety-storage/search/safety-storage-modal.tsx
+++ app/storage/components/safety-storage/search/safety-storage-modal.tsx
@@ -1,7 +1,30 @@
-export const SafetyStorageModal = () => {
+export const SafetyStorageModal = ({
+	safetyStoragePercentage
+}: {
+	safetyStoragePercentage: number
+}) => {
 	return (
-		<div>
-			<div></div>
+		<div className="w-36 h-full py-6 flex flex-col justify-center place-items-center space-y-3">
+			{safetyStoragePercentage > 1.1 ? (
+				<>
+					<div className="w-full h-full rounded-full bg-good" />
+					<div className="w-full h-full rounded-full bg-good" />
+					<div className="w-full h-full rounded-full bg-good" />
+				</>
+			) : safetyStoragePercentage < 0.9 ? (
+				<>
+					<div className="w-full h-full rounded-full bg-white" />
+					<div className="w-full h-full rounded-full bg-white" />
+					<div className="w-full h-full rounded-full bg-secondary" />
+				</>
+			) : (
+				<>
+					<div className="w-full h-full rounded-full bg-white" />
+					<div className="w-full h-full rounded-full bg-warning" />
+					<div className="w-full h-full rounded-full bg-warning" />
+				</>
+			)}
+			<div className="font-bold">倉庫 1</div>
 		</div>
 	)
 }
