0f13afe062485addd6e65c011efb920fb0b964c8
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-22 00:31:04 +0800

    feat(safety-storage): add min-height to component

--- app/storage/components/safety-storage/search/safety-storage.tsx
+++ app/storage/components/safety-storage/search/safety-storage.tsx
@@ -2,16 +2,17 @@ import { SafetyStorageModal, SafetyStorageSearch } from "."
 
 export const SafetyStorage = () => {
 	return (
-		<div className="w-full h-max flex flex-col justify-start place-items-start rounded-[32px] bg-base/90">
-			<div className="w-full flex justify-between place-items-center px-7 pt-7">
+		<div className="w-full h-max min-h-[400px] p-7 flex flex-col justify-start place-items-start rounded-[32px] bg-base/90">
+			<div className="w-full flex justify-between place-items-center">
 				<div className="w-full h-max -space-y-1 font-bold mb-4">
 					<div className="text-2xl">安全庫存查詢</div>
 					<div className="text-sm">資料時間: 3204/2/30 25:61</div>
         </div>
         <SafetyStorageSearch />
-      </div>
+			</div>
+			<div className="font-bold py-2">商品名稱: </div>
       <div className="w-full h-full flex justify-start place-items-center space-x-4 overflow-y-scroll pl-7">
-        <SafetyStorageModal />
+        <SafetyStorageModal safetyStoragePercentage={1} />
       </div>
 		</div>
 	)
