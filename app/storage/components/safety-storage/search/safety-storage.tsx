c4864f7047ed4566b9aca3ad0b53358022b90279
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-23 00:25:01 +0800

    feat(safety-storage): update padding to component

--- app/storage/components/safety-storage/search/safety-storage.tsx
+++ app/storage/components/safety-storage/search/safety-storage.tsx
@@ -2,9 +2,9 @@ import { SafetyStorageModal, SafetyStorageFilter } from "."
 
 export const SafetyStorage = () => {
 	return (
-		<div className="w-full h-max min-h-[400px] p-7 flex flex-col justify-start place-items-start rounded-[32px] bg-base/90">
-			<div className="w-full flex justify-between place-items-center">
-				<div className="w-full h-max -space-y-1 font-bold mb-4">
+		<div className="w-full h-max min-h-[400px] py-7 flex flex-col justify-start place-items-start rounded-[32px] bg-base/90">
+			<div className="w-full flex justify-between place-items-center px-7 mb-4">
+				<div className="w-full h-max -space-y-1 font-bold">
 					<div className="text-2xl">安全庫存查詢</div>
 					<div className="text-sm">資料時間: 3204/2/30 25:61</div>
 				</div>
