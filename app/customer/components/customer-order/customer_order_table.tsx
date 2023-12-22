7143a7a7f69e979d031c23b6158a48a00866eab7
tintin <abctintin0504@gmail.com>
2023-12-22 21:21:35 +0800

    add a dropdown menu to search customer order

--- app/customer/components/customer-order/customer_order_table.tsx
+++ app/customer/components/customer-order/customer_order_table.tsx
@@ -20,39 +20,64 @@ import {
     getSortedRowModel,
     useReactTable
 } from "@tanstack/react-table"
-import { useState } from "react"
+import { SetStateAction, useState } from "react"
 // util
 import { Button } from "@/components/ui/button"
 import { columns } from "./columns"
 
 // TODO: REPLACE FAKE DATA
+const product: ProductInCustomerOrder[] = [
+    {
+        productId: "p1",
+        price: 300,
+        amount: 2,
+    },
+    {
+        productId: "p2",
+        price: 600,
+        amount: 1,
+    },
+    {
+        productId: "p3",
+        price: 550,
+        amount: 1,
+    },
+]
 const data: CustomerOrder[] = [
 	{
 		orderId: "t1",
-		customerId: "c1",
+		customerId: "c2",
 		createdAt: "2023/04/08 04:08",
 		price: 520,
-		products: [],
+		// products: [product[0],product[2]],
+        products: [],
 	},
 	{
 		orderId: "t2",
 		customerId: "c1",
 		createdAt: "2023/04/08 08:08",
 		price: 520,
-		products: [],
+		// products: [product[0]],
+        products: [],
 	},
 	{
 		orderId: "t3",
-		customerId: "c1",
+		customerId: "c450",
 		createdAt: "2023/04/08 14:08",
 		price: 520,
-		products: [],
+		// products: product,
+        products: [],
 	}
 ]
 
 export const CustomerOrderTable = () => {
 	const [sorting, setSorting] = useState<SortingState>([])
 	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+    const [filterField, setFilterField] = useState("orderId")
+
+    const handleFilterFieldChange = (event: { target: { value: SetStateAction<string> } }) => {
+        setFilterField(event.target.value);
+    };
 
 	const table = useReactTable({
 		data,
@@ -80,7 +105,7 @@ export const CustomerOrderTable = () => {
 				>
 					Previous
 				</Button>
-				<Button onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+				<Button onClick={() => {console.log("clickkkkk"); table.nextPage();}} disabled={!table.getCanNextPage()}>
 					Next
 				</Button>
 			</div>
@@ -91,12 +116,24 @@ export const CustomerOrderTable = () => {
 		<div className="w-full h-full p-7 space-y-4 bg-white rounded-[32px]">
             <div className="flex justify-end place-items-center">
 				{/* <div className="pl-1 font-bold">商品名稱: </div> */}
-				<Input
-					placeholder="搜尋倉庫編號..."
-					value={(table.getColumn("orderId")?.getFilterValue() as string) ?? ""}
-					onChange={(event) => table.getColumn("orderId")?.setFilterValue(event.target.value)}
-					className="w-1/3"
-				/>
+                <select
+                    value={filterField}
+                    onChange={handleFilterFieldChange}
+                    className="p-2 border rounded"
+                    // className="p-2 rounded-2xl border"
+                >
+                    <option value="orderId">訂單編號</option>
+                    <option value="customerId">顧客編號</option>
+                    <option value="createdAt">下單時間</option>
+                    <option value="product">購買產品</option>
+                </select>
+                <div style={{ width: "15px" }}></div>
+                <Input
+                    placeholder={`search ${filterField}...`}
+                    value={(table.getColumn(filterField)?.getFilterValue() as string) ?? ""}
+                    onChange={(event) => table.getColumn(filterField)?.setFilterValue(event.target.value)}
+                    className="w-1/3"
+                />
 			</div>
 			<div className="rounded-2xl border">
 				<Table>
