50f189b54e57f6ff8b18ebb49d4987be2a52a6cc
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-22 21:53:28 +0800

    feat(safety-storage-filter): rebuild input with select

---
+++ app/storage/components/safety-storage/search/safety-storage-filter.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue
+} from "@/components/ui/select"
+
+export const SafetyStorageFilter = () => {
+	return (
+		<div className="w-1/3 flex flex-col justify-end place-items-start space-x-2">
+			<div className="pl-2">Select status to display.</div>
+			<Select defaultValue="all">
+				<SelectTrigger>
+					<SelectValue placeholder={"Select status to display."} />
+				</SelectTrigger>
+				<SelectContent className="bg-background">
+					<SelectItem value="all">All.</SelectItem>
+					<SelectItem value="good">Good.</SelectItem>
+					<SelectItem value="warning">Warning.</SelectItem>
+					<SelectItem value="bad">Bad.</SelectItem>
+				</SelectContent>
+			</Select>
+		</div>
+	)
+}
