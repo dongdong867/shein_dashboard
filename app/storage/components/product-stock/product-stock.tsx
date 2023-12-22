009029f24592ca3ff30b6cb341971df57e322a33
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-23 00:25:55 +0800

    fix(product-stock): remove error margin bottom class

--- app/storage/components/product-stock/product-stock.tsx
+++ app/storage/components/product-stock/product-stock.tsx
@@ -5,7 +5,7 @@ export const ProductStock = () => {
 	return (
 		<div className="w-full h-max flex flex-col justify-start place-items-start rounded-[32px] bg-base/90 overflow-hidden">
 			<div className="w-full flex justify-between place-items-center px-7 pt-7">
-				<div className="w-full h-max -space-y-1 font-bold mb-4">
+				<div className="w-full h-max -space-y-1 font-bold">
 					<div className="text-2xl">商品庫存查詢</div>
 					<div className="text-sm">資料時間: {" "} 3204/2/30 25:61</div>
 				</div>
