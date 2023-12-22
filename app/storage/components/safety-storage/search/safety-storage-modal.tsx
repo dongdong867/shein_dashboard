f590759ad3b32fe31375bf22459842964c961eb1
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-22 14:57:52 +0800

    feat(safety-storage-modal): add product name display and id in tooltip

--- app/storage/components/safety-storage/search/safety-storage-modal.tsx
+++ app/storage/components/safety-storage/search/safety-storage-modal.tsx
@@ -1,10 +1,19 @@
-export const SafetyStorageModal = ({
-	safetyStoragePercentage
-}: {
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+import { PiQuestionFill } from "react-icons/pi"
+
+type SafetyStorageModalProps = {
 	safetyStoragePercentage: number
-}) => {
+	productId: string
+	productName: string
+}
+
+export const SafetyStorageModal = ({
+	safetyStoragePercentage,
+	productId,
+	productName
+}: SafetyStorageModalProps) => {
 	return (
-		<div className="w-36 h-full py-4 flex flex-col justify-center place-items-center space-y-3">
+		<div className="w-40 min-w-[144px] h-full pt-4 flex flex-col justify-center place-items-center space-y-3">
 			{safetyStoragePercentage > 1.1 ? (
 				<>
 					<div className="w-full h-full rounded-full bg-good" />
@@ -24,7 +33,21 @@ export const SafetyStorageModal = ({
 					<div className="w-full h-full rounded-full bg-warning" />
 				</>
 			)}
-			<div className="font-bold">倉庫 1</div>
+			<div className="font-bold text-center">
+				<TooltipProvider>
+					<Tooltip>
+						<TooltipTrigger>
+							<div className="flex justify-center place-items-center gap-x-1">
+								{productName}
+								<PiQuestionFill size={16} />
+							</div>
+						</TooltipTrigger>
+						<TooltipContent className="bg-background">
+							<div className="text-xs">{productId}</div>
+						</TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
+			</div>
 		</div>
 	)
 }
