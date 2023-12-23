0403671300206c859d98967b70fc7914beb39593
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-23 10:35:10 +0800

    feat(stock-status): implement database connecting

--- app/storage/components/stock-status/stock-status.tsx
+++ app/storage/components/stock-status/stock-status.tsx
@@ -1,7 +1,10 @@
+import { getStockStatus } from "@/actions/stock-status"
 import { StockStatusModal } from "."
 import StockStatusReloadButton from "./reload-button"
 
-export const StockStatus = () => {
+export const StockStatus = async () => {
+	const stockStatus = await getStockStatus()
+
 	return (
 		<div className="w-full h-max min-h-[320px] flex flex-col justify-start place-items-start rounded-[32px] bg-base/90">
 			<div className="w-full flex justify-between place-items-center px-7 pt-7 mb-2">
@@ -12,7 +15,17 @@ export const StockStatus = () => {
 				<StockStatusReloadButton />
 			</div>
 			<div className="w-full h-full flex justify-start place-items-center space-x-4 overflow-y-scroll pl-7">
-				<StockStatusModal percentage={100} name="test" />
+				{stockStatus.length > 0 ? (
+					stockStatus.map((status) => (
+						<StockStatusModal
+							key={status.id}
+							percentage={status.stock / status.capacity * 100}
+							name={status.name}
+						/>
+					))
+				) : (
+					<div className="pt-6 text-center text-base font-medium">No factory order found.</div>
+				)}
 			</div>
 		</div>
 	)
