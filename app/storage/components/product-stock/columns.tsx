0a4ef56358447f5ba0cf92774e03983b9073cb84
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-24 10:12:11 +0800

    fix(product-stock): add m-auto to header button

--- app/storage/components/product-stock/columns.tsx
+++ app/storage/components/product-stock/columns.tsx
@@ -4,6 +4,8 @@ import { Column, ColumnDef, SortDirection } from "@tanstack/react-table"
 import { Button } from "@/components/ui/button"
 // icons
 import { LuArrowDown01, LuArrowDown10, LuArrowDownUp } from "react-icons/lu"
+// types
+import { ProductStock } from "@/types/product-stock"
 
 const getSortingArrowIcon = (isSorted: false | SortDirection) => {
 	return isSorted ? isSorted == "asc" ? <LuArrowDown01 /> : <LuArrowDown10 /> : <LuArrowDownUp />
@@ -23,36 +25,40 @@ const SortButton = ({
 	children: React.ReactNode
 }) => {
 	return (
-		<div className="flex justify-center">
-			<Button
-				variant={"ghost"}
-				onClick={() => sortHandler(column.getIsSorted(), column)}
-				className="text-center hover:text-primary hover:bg-primary/10"
-			>
-				{children}
-				{getSortingArrowIcon(column.getIsSorted())}
-			</Button>
-		</div>
+		<Button
+			variant={"ghost"}
+			onClick={() => sortHandler(column.getIsSorted(), column)}
+			className="px-2 text-center hover:text-primary hover:bg-primary/10"
+		>
+			{children}
+			{getSortingArrowIcon(column.getIsSorted())}
+		</Button>
 	)
 }
 
 export const columns: ColumnDef<ProductStock>[] = [
 	{
 		accessorKey: "storageId",
-		header: "倉庫編號",
-		cell: ({ row }) => <div className="lowercase pl-1">{row.getValue("storageId")}</div>
+		header: ({ column }) => (
+			<div className="pl-4">
+				倉庫編號
+			</div>
+		),
+		cell: ({ row }) => <div className="pl-1">{row.getValue("storageId")}</div>
 	},
 	{
 		accessorKey: "storageName",
-		header: "倉庫名稱",
+		header: ({ column }) => <div className="whitespace-nowrap">倉庫名稱</div>,
 		cell: ({ row }) => <div className="lowercase">{row.getValue("storageName")}</div>
 	},
 	{
 		accessorKey: "averageShipping",
 		header: ({ column }) => (
-			<SortButton column={column}>
-				<span className="pr-2">平均出貨量</span>
-			</SortButton>
+			<div className="w-max m-auto">
+				<SortButton column={column}>
+					<span>平均出貨量</span>
+				</SortButton>
+			</div>
 		),
 		cell: ({ row }) => (
 			<div className="lowercase text-center">{row.getValue("averageShipping")}</div>
@@ -61,18 +67,24 @@ export const columns: ColumnDef<ProductStock>[] = [
 	{
 		accessorKey: "periodCount",
 		header: ({ column }) => (
-			<SortButton column={column}>
-				<span className="pr-2">累積期數</span>
-			</SortButton>
+			<div className="w-max m-auto">
+				<SortButton column={column}>
+					<span className="pr-2">累積期數</span>
+				</SortButton>
+			</div>
 		),
-		cell: ({ row }) => <div className="lowercase text-center">{row.getValue("periodCount")}</div>
+		cell: ({ row }) => (
+			<div className="w-max m-auto lowercase text-center">{row.getValue("periodCount")}</div>
+		)
 	},
 	{
 		accessorKey: "amount",
 		header: ({ column }) => (
-			<SortButton column={column}>
-				<span className="pr-2">庫存數量</span>
-			</SortButton>
+			<div className="w-max m-auto">
+				<SortButton column={column}>
+					<span className="pr-2">庫存數量</span>
+				</SortButton>
+			</div>
 		),
 		cell: ({ row }) => <div className="lowercase text-center">{row.getValue("amount")}</div>
 	}
