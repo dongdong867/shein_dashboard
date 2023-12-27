5e28a8a1e658cf065d4664eee3457f529613ddd5
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-27 09:28:49 +0800

    feat(safety-storage): add insert handler to component

--- app/storage/components/safety-storage/analysis/safety-storage-analysis-insert.tsx
+++ app/storage/components/safety-storage/analysis/safety-storage-analysis-insert.tsx
@@ -1,14 +1,38 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
+// components
 import { Input } from "@/components/ui/input"
-import { FaPlus } from "react-icons/fa6";
+import { Button } from "@/components/ui/button"
+// icons
+import { FaPlus } from "react-icons/fa6"
+import { useState } from "react"
 
-export const SafetyStorageAnalysisInsert = () => {
+export const SafetyStorageAnalysisInsert = ({
+	handleIdInsert
+}: {
+	handleIdInsert: (insertedId: string) => void
+}) => {
+	const [productId, setProductId] = useState("")
+
+	const handleClick = () => {
+		handleIdInsert(productId)
+		setProductId("")
+	}
+
 	return (
 		<div className="w-full flex justify-end place-items-center space-x-2">
-			<Input placeholder="輸入商品編號..." className="w-full xl:w-3/5 px-4" />
-			<Button variant={"default"} asChild className="w-[32.5px] h-[32.5px] p-1 cursor-pointer">
+			<Input
+				placeholder="輸入商品編號..."
+				value={productId}
+				onChange={(e) => setProductId(e.target.value)}
+				className="w-full xl:w-3/5 px-4"
+			/>
+			<Button
+				onClick={handleClick}
+				variant={"default"}
+				asChild
+				className="w-[32.5px] h-[32.5px] p-1 cursor-pointer"
+			>
 				<FaPlus size={40} />
 			</Button>
 		</div>
