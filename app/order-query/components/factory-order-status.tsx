cae727ddaba9a94fc94f2fa153c56fcd8da7ffa9
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-28 14:08:58 +0800

    fix(factory-order-status): rebuild component searching logic

--- app/order-query/components/factory-order-status.tsx
+++ app/order-query/components/factory-order-status.tsx
@@ -1,66 +1,51 @@
 "use client"
 
 // types
-import { FactoryOrder } from "@/types/factory-order";
-import { Factory } from "@/types/factory";
+import { FactoryOrder } from "@/types/factory-order"
+import { Factory } from "@/types/factory"
 
-import React, { useEffect, useState } from "react";
-import { FactoryDetail } from "./factory-detail";
-import { OrderSearch } from "./order-search";
-import { getFactory } from "@/actions/factory";
-import { getFactoryOrders } from "@/actions/factory-order";
+import React, { useState } from "react"
+import { FactoryDetail } from "./factory-detail"
+import { OrderSearch } from "./order-search"
 
+export const FactoryOrderStatus = ({
+	factory,
+	orders
+}: {
+	factory: Factory[]
+	orders: FactoryOrder[]
+}) => {
+	const [searchOrderId, setSearchOrderId] = useState("")
+	const [searchFactoryName, setSearchFactoryName] = useState("")
 
-export const FactoryOrderStatus = () => {      
-  const [factory, setData] = useState<Factory[]>([]);
-  const [orders, setOrders] = useState<FactoryOrder[]>([]);
-  const [searchParams, setSearchParams] = useState({});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const factoryData = await getFactory();
-        setData(factoryData);
-
-        const factoryOrderData = await getFactoryOrders(); 
-        setOrders(factoryOrderData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-  
-  const handleSearch = (params: any) => {
-    setSearchParams(params);
-  };
-  
-  return (
-    <div className="w-full h-max space-y-4">
-      <OrderSearch onSearch={handleSearch} />
-      <div className="w-full h-max flex flex-col justify-start place-items-start rounded-[32px] bg-base/90 overflow-hidden">
-      <div className="w-full flex justify-between place-items-center px-7 pt-7">
-        <div className="w-full h-max -space-y-1 font-bold mb-4">
-          <div className="text-2xl">工廠資訊及生產進度</div>
-          <div className="w-full h-full p-7 flex flex-wrap gap-6">
-            {factory.map((factory) => {
-              const factoryOrders = orders.filter((o) =>
-                  factory.schedule.some((schedule) => schedule.orderId === o.orderId)
-              );
-              return (
-                <FactoryDetail 
-                  key={factory.id} 
-                  factory={factory} 
-                  order={factoryOrders} 
-                  searchParams={searchParams}
-                />
-              );
-            })}
-          </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className="w-full h-max space-y-4">
+			<OrderSearch
+				setSearchFactoryName={setSearchFactoryName}
+				setSearchOrderId={setSearchOrderId}
+			/>
+			<div className="w-full h-max flex flex-col justify-start place-items-start rounded-[32px] bg-base/90 overflow-hidden">
+				<div className="w-full flex justify-between place-items-center px-7 pt-7">
+					<div className="w-full h-max -space-y-1 font-bold mb-4">
+						<div className="text-2xl">工廠資訊及生產進度</div>
+						<div className="w-full h-full p-7 flex flex-wrap gap-6">
+							{factory.map((factory) => {
+								if (
+									searchFactoryName == "" ||
+									searchOrderId == "" ||
+									(factory.name == searchFactoryName &&
+										factory.schedule.some((order) => order.orderId === searchOrderId))
+								) {
+									const factoryOrders = orders.filter((o) =>
+										factory.schedule.some((schedule) => schedule.orderId === o.orderId)
+									)
+									return <FactoryDetail key={factory.id} factory={factory} order={factoryOrders} />
+								}
+							})}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
