bb4d2ce1bf2f0e75fb9a291cde69314f0b0361da
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-12-28 14:07:40 +0800

    fix(order-search): fix search param handling logic

--- app/order-query/components/order-search.tsx
+++ app/order-query/components/order-search.tsx
@@ -1,57 +1,60 @@
 "use client"
 
-import React, { useState } from "react";
+// hooks
+import React, { useState } from "react"
+// icons
 import { IoSearch } from "react-icons/io5"
+// components
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 type OrderSearchProps = {
-	onSearch: (searchParams: { factoryName?: string; orderId?: string }) => void;
-};
-  
-export const OrderSearch: React.FC<OrderSearchProps> = ({ onSearch }) => {
-	const [searchParams, setSearchParams] = useState({
-		factoryName: "",
-		orderId: "",
-	});
-	
-	const handleSearch = () => {
-		onSearch(searchParams);
-	};
+	setSearchOrderId: React.Dispatch<React.SetStateAction<string>>
+	setSearchFactoryName: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const OrderSearch = ({ setSearchFactoryName, setSearchOrderId }: OrderSearchProps) => {
+	const [factoryName, setFactoryName] = useState("")
+	const [orderId, setOrderId] = useState("")
 
 	return (
 		<div className="flex flex-row justify-between place-items-center border-gray-300">
-      		<div className="w-full flex place-items-center space-x-2">
-				<Input 
-					placeholder="輸入工廠名稱..." 
-					className="w-full xl:w-1/2" 
-					value={searchParams.factoryName}
-          			onChange={(e) => setSearchParams({ ...searchParams, factoryName: e.target.value })}
+			<div className="w-full flex place-items-center space-x-2">
+				<Input
+					placeholder="輸入工廠名稱..."
+					className="w-full xl:w-1/2"
+					onChange={(e) => setFactoryName(e.target.value)}
 				/>
-				<Button 
-					variant={"default"} 
-					asChild 
+				<Button
+					variant={"default"}
+					asChild
 					className="w-[32.5px] h-[32.5px] p-1 cursor-pointer"
-				  	onClick={handleSearch}>
+					onClick={() => {
+						setSearchFactoryName(factoryName)
+						setFactoryName("")
+					}}
+				>
 					<IoSearch size={40} />
 				</Button>
 			</div>
 			<div className="w-full flex place-items-center space-x-2">
-				<Input 
-					placeholder="輸入訂單編號..." 
+				<Input
+					placeholder="輸入訂單編號..."
 					className="w-full xl:w-1/2"
-					value={searchParams.orderId}
-          			onChange={(e) => setSearchParams({ ...searchParams, orderId: e.target.value })}
-					/>
-				<Button 
-					variant={"default"} 
-					asChild 
+					onChange={(e) => setOrderId(e.target.value)}
+				/>
+				<Button
+					variant={"default"}
+					asChild
 					className="w-[32.5px] h-[32.5px] p-1 cursor-pointer"
-					onClick={handleSearch}
-					>
+					onClick={() => {
+						setSearchOrderId(orderId)
+						setOrderId("")
+					}}
+				>
 					<IoSearch size={40} />
 				</Button>
 			</div>
-    	</div>
+		</div>
 	)
 }
