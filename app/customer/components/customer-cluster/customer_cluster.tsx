86cb34ad57184ce05cc8b62a19234b8620a6338d
tintin <abctintin0504@gmail.com>
2023-12-29 11:28:31 +0800

    go to real time

--- app/customer/components/customer-cluster/customer_cluster.tsx
+++ app/customer/components/customer-cluster/customer_cluster.tsx
@@ -4,13 +4,13 @@ import { AgeClusterResult } from "./age_cluster_result";
 import { LocationClusterResult } from "./location_cluster_result";
 import RFM from "./rfm";
 
-export const CustomerCluster = () => {
+export const CustomerCluster = ({time}: any & String) => {
 	return (
 		<div className="w-full h-max flex flex-col justify-start place-items-start rounded-[32px] bg-base/90 overflow-hidden">
 			<div className="w-full flex justify-between place-items-center px-7 pt-7">
 				<div className="w-full h-max -space-y-1 font-bold mb-4">
 					<div className="text-2xl">顧客分群分析</div>
-					<div className="text-sm">資料時間: {" "} 3204/2/30 25:61</div>
+					<div className="text-sm">資料時間: {time} </div>
 				</div>
 			</div>
 			<div className="w-full p-7">
