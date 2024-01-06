23b8cc5b76ca053acab1944099ca025f61765ab4
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-06 18:36:54 +0800

    feat(sales-chart): change chart package to nivo

--- app/production/components/production/production-analysis.tsx
+++ app/production/components/production/production-analysis.tsx
@@ -1,128 +1,62 @@
 "use client"
-import React, { PureComponent } from 'react';
-import { ReferenceArea } from 'recharts';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { ResponsiveLine, Serie } from "@nivo/line"
 
-const data = [
-{
-  period: '2022Q1',
-  totalSales: 102684,
-  totalIncome: 169241450,
-},
-{
-  period: '2022Q2',
-  totalSales: 87991,
-  totalIncome: 149187290,
-},
-{
-  period: '2022Q3',
-  totalSales: 93796,
-  totalIncome: 162826160,
-},
-{
-  period: '2022Q4',
-  totalSales: 102004,
-  totalIncome: 187625000,
-},
-{
-  period: '2023Q1',
-  totalSales: 98306,
-  totalIncome: 171989590,
-},
-{
-  period: '2023Q2',
-  totalSales: 87728,
-  totalIncome: 141332240,
-},
-{
-  period: '2023Q3',
-  totalSales: 90595,
-  totalIncome: 147389820,
-},
-{
-  period: '2023Q4',
-  totalSales: 110527,
-  totalIncome: 213870125,
-},
-];
+const salesData: Serie[] = [
+	{
+		id: "Total Sales",
+		data: [
+			{ x: "2022/Q1", y: 102684 },
+			{ x: "2022/Q2", y: 87991 },
+			{ x: "2022/Q3", y: 93796 },
+			{ x: "2022/Q4", y: 102004 },
+			{ x: "2023/Q1", y: 98306 },
+			{ x: "2023/Q2", y: 87728 },
+			{ x: "2023/Q3", y: 90595 },
+			{ x: "2023/Q4", y: 110527 }
+		]
+	}
+]
 
+const incomeData: Serie[] = [
+  {
+    id: " Total Income",
+    data: [
+      { x: "2022/Q1", y: 169241450 },
+			{ x: "2022/Q2", y: 149187290 },
+			{ x: "2022/Q3", y: 162826160 },
+			{ x: "2022/Q4", y: 187625000 },
+			{ x: "2023/Q1", y: 171989590 },
+			{ x: "2023/Q2", y: 141332240 },
+			{ x: "2023/Q3", y: 147389820 },
+			{ x: "2023/Q4", y: 213870125 }
+    ]
+  }
+]
 
+export const TotalSalesChart = () => {
+	return (
+    <ResponsiveLine
+      data={salesData}
+      yScale={{type: "linear", min: 85000}}
+      colors={{ scheme: "category10" }}
+			margin={{ top: 40, right: 30, bottom: 30, left: 50 }}
+			useMesh={true}
+			motionConfig={"gentle"}
+      pointSize={8}
+    />
+	)
+}
 
-const TotalSalesChart = () => {
-return (
-  <ResponsiveContainer width="90%" height={300}>
-    <LineChart
-      data={data}
-      margin={{
-        top: 40,
-        right: 10,
-        left: 10,
-        bottom: 40,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="period" tick={{ fontSize: 12 }}/>
-      <YAxis domain={['auto', 'auto']} />
-      <Tooltip />
-      <Legend />
-
-      <Line
-        type="monotone"
-        dataKey="totalSales"
-        name="Total Sales"
-        stroke=" #0066FF"
-        activeDot={{ r: 8 }}
-      />
-    </LineChart>
-  </ResponsiveContainer>
-);
-};
-
-const TotalIncomeChart = () => {
-return (
-  <ResponsiveContainer width="90%" height={300}>
-    <LineChart
-      data={data}
-      margin={{
-        top: 40,
-        right: 10,
-        left: 50,
-        bottom: 40,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="period" tick={{ fontSize: 12 }}/>
-      <YAxis domain={['auto', 'auto']} />
-      <Tooltip />
-      <Legend />
-
-      <Line
-        type="monotone"
-        dataKey="totalIncome"
-        name="Total Income"
-        stroke= "#0066FF"
-        activeDot={{ r: 8 }}
-      />
-    </LineChart>
-  </ResponsiveContainer>
-);
-};
-
-const Chart = () => {
-const titleStyle = { fontSize: '24px', fontWeight: 'bold', color: 'black' }; 
-
-  return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ flex: 1 }}>
-        <h2 style={titleStyle}>Total Sales</h2>
-        <TotalSalesChart />
-      </div>
-      <div style={{ flex: 1 }}>
-        <h2 style={titleStyle}>Total Income</h2>
-        <TotalIncomeChart />
-      </div>
-    </div>
-  );
-};
-
-export default Chart;
+export const TotalIncomeChart = () => {
+	return (
+		<ResponsiveLine
+      data={incomeData}
+      yScale={{type: "linear", min: 130000000}}
+      colors={{ scheme: "category10" }}
+			margin={{ top: 40, right: 30, bottom: 30, left: 80 }}
+			useMesh={true}
+			motionConfig={"gentle"}
+      pointSize={8}
+    />
+	)
+}
