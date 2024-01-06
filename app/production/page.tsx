7a2ab1ccd3e5cd3c85ef8c62d398518934b2a35e
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-06 21:16:25 +0800

    feat(production): update chart components

--- app/production/page.tsx
+++ app/production/page.tsx
@@ -11,55 +11,37 @@ const ProductionPage = () => {
 				<div className="text-4xl">生產量管理系統</div>
 			</div>
 
-			<div className="relative w-full h-max bg-white/80 p-7 rounded-[32px] z-20">
-				<div className="text-3xl font-bold text-center">Market Trends</div>
-				<div className="h-[500px]">
+			<div className="relative w-full h-max bg-base/90 p-7 rounded-[32px] z-20">
+				<div className="text-3xl font-bold pb-4">Market Trends</div>
+				<div className="h-[500px] p-4 bg-background rounded-3xl">
 					<TrendChart />
 				</div>
 			</div>
 
 			<div className="w-full flex space-x-8">
-				<div className="w-full h-max bg-white/80 rounded-[32px] p-7">
-					<div className="text-2xl font-bold">Total Sales</div>
-					<div className="h-[300px]">
+				<div className="w-full h-max bg-base/90 rounded-[32px] p-7">
+					<div className="text-2xl font-bold pb-4">Total Sales</div>
+					<div className="w-full h-[300px] p-4 bg-background rounded-3xl">
 						<TotalSalesChart />
 					</div>
 				</div>
 
-				<div className="w-full h-max bg-white/80 rounded-[32px] p-7">
-					<div className="text-2xl font-bold">Total Income</div>
-					<div className="w-full h-[300px]">
+				<div className="w-full h-max bg-base/90 rounded-[32px] p-7">
+					<div className="text-2xl font-bold pb-4">Total Income</div>
+					<div className="w-full h-[300px] p-4 bg-background rounded-3xl">
 						<TotalIncomeChart />
 					</div>
 				</div>
 			</div>
 
-			{/* GrowthChart 的容器 */}
-			<div className="relative bg-white/80 p-4 rounded-[16px] overflow-auto z-10">
-				{/* GrowthChart 元件 */}
-				<GrowthChart />
+			<div className="relative bg-base/90 p-7 rounded-[32px] z-10">
+				<div className="text-2xl font-bold pb-4">Growth Rate</div>
+				<div className="w-full h-[350px] p-4 bg-background rounded-3xl">
+					<GrowthChart />
+				</div>
 			</div>
 		</div>
 	)
 }
 
 export default ProductionPage
-
-// const ProductionPage = () => {
-// 	return (
-// 		<div className="w-full h-full p-10 space-y-8 flex flex-col justify-start bg-neutral-500/90 rounded-[32px] overflow-auto"		>
-// 			<div className="text-primary-foreground font-bold space-y-2">
-// 				<div className="text-sm">SHEIN 數位轉型計畫</div>
-// 				<div className="text-4xl">生產量管理系統</div>
-// 				<div style={{height: "100px"}}>
-// 				<TrendChart />
-// 				<Chart/>
-// 				<GrowthChart/>
-// 				<div/>
-// 			</div>
-// 			</div>
-// 		</div>
-// 	)
-// }
-
-// export default ProductionPage
