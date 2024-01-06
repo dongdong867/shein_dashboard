9247ffe1f4372305b18c38817c744f5ef10d0793
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-07 02:54:08 +0800

    feat(customer-order): update time type

--- app/customer/components/customer-order/customer_order.tsx
+++ app/customer/components/customer-order/customer_order.tsx
@@ -1,15 +1,12 @@
-// import { ProductStockSearch } from "./product-stock-search"
-// import { ProductStockTable } from "./product-stock-table"
-
 import { CustomerOrderTable } from "./customer_order_table"
 
-export const CustomerOrder = ({time}: any & String) => {
+export const CustomerOrder = ({ time }: { time: string }) => {
 	return (
 		<div className="w-full h-max flex flex-col justify-start place-items-start rounded-[32px] bg-base/90 overflow-hidden">
 			<div className="w-full flex justify-between place-items-center px-7 pt-7">
 				<div className="w-full h-max -space-y-1 font-bold mb-4">
 					<div className="text-2xl">顧客訂單查詢</div>
-					<div className="text-sm">資料時間: {time} </div>
+					<div className="text-sm">資料時間: {time}</div>
 				</div>
 			</div>
 			<div className="w-full h-full p-7">
