70193492a7e690c706d05b883fa9061076bf4b3d
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-07 03:35:11 +0800

    feat(customer-order-table): update Select usage and pageSize

--- app/customer/components/customer-order/customer_order_table.tsx
+++ app/customer/components/customer-order/customer_order_table.tsx
@@ -13,6 +13,7 @@ import {
 import {
 	ColumnFiltersState,
 	SortingState,
+	Table as TanstackTable,
 	flexRender,
 	getCoreRowModel,
 	getFilteredRowModel,
@@ -20,29 +21,19 @@ import {
 	getSortedRowModel,
 	useReactTable
 } from "@tanstack/react-table"
-import { SetStateAction, useState } from "react"
+import { useEffect, useState } from "react"
 // util
 import { Button } from "@/components/ui/button"
 import { columns } from "./columns"
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue
+} from "@/components/ui/select"
 
 // TODO: REPLACE FAKE DATA
-const product: ProductInCustomerOrder[] = [
-    {
-        productId: "p1",
-        price: 300,
-        amount: 2,
-    },
-    {
-        productId: "p2",
-        price: 600,
-        amount: 1,
-    },
-    {
-        productId: "p3",
-        price: 550,
-        amount: 1,
-    },
-]
 const data: CustomerOrder[] = [
 	{
 		orderId: "t1",
@@ -50,7 +41,7 @@ const data: CustomerOrder[] = [
 		createdAt: "2023/04/08 04:08",
 		price: 524,
 		// products: [product[0],product[2]],
-        products: "毛衣x2 運動褲x1 項鍊x5",
+		products: "毛衣x2 運動褲x1 項鍊x5"
 	},
 	{
 		orderId: "t2",
@@ -58,7 +49,7 @@ const data: CustomerOrder[] = [
 		createdAt: "2023/04/08 08:08",
 		price: 628,
 		// products: [product[0]],
-        products: "上衣x2 褲子x1 鞋子x1",
+		products: "上衣x2 褲子x1 鞋子x1"
 	},
 	{
 		orderId: "t3",
@@ -66,7 +57,7 @@ const data: CustomerOrder[] = [
 		createdAt: "2023/04/08 14:08",
 		price: 552,
 		// products: product,
-        products: "外套x1 項鍊x3",
+		products: "外套x1 項鍊x3"
 	},
 	{
 		orderId: "t4",
@@ -90,36 +81,56 @@ const data: CustomerOrder[] = [
 		products: "運動裝x1"
 	},
 	{
-		"orderId": "t7",
-		"customerId": "Eva",
-		"createdAt": "2023/04/09 20:15",
-		"price": 550,
-		"products": "外套x2 毛衣x1 裙子x1"
+		orderId: "t7",
+		customerId: "Eva",
+		createdAt: "2023/04/09 20:15",
+		price: 550,
+		products: "外套x2 毛衣x1 裙子x1"
 	},
 	{
-		"orderId": "t8",
-		"customerId": "Alice",
-		"createdAt": "2023/04/09 23:55",
-		"price": 1410,
-		"products": "休閒鞋x2"
+		orderId: "t8",
+		customerId: "Alice",
+		createdAt: "2023/04/09 23:55",
+		price: 1410,
+		products: "休閒鞋x2"
 	},
 	{
-		"orderId": "t9",
-		"customerId": "Carson",
-		"createdAt": "2023/04/10 11:20",
-		"price": 620,
-		"products": "睡衣x2"
+		orderId: "t9",
+		customerId: "Carson",
+		createdAt: "2023/04/10 11:20",
+		price: 620,
+		products: "睡衣x2"
 	}
 ]
+
+const PaginationButton = ({ table }: { table: TanstackTable<CustomerOrder> }) => {
+	return (
+		<div className="flex justify-end space-x-2">
+			<Button
+				variant={"outline"}
+				onClick={() => table.previousPage()}
+				disabled={!table.getCanPreviousPage()}
+				className="hover:bg-primary-foreground hover:text-primary"
+			>
+				Previous
+			</Button>
+			<Button
+				onClick={() => {
+					console.log("next page button clicked")
+					table.nextPage()
+				}}
+				disabled={!table.getCanNextPage()}
+			>
+				Next
+			</Button>
+		</div>
+	)
+}
 
 export const CustomerOrderTable = () => {
 	const [sorting, setSorting] = useState<SortingState>([])
 	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
-    const [filterField, setFilterField] = useState("orderId")
-
-    const handleFilterFieldChange = (event: { target: { value: SetStateAction<string> } }) => {
-        setFilterField(event.target.value);
-    };
+	const [filterField, setFilterField] = useState("orderId")
 
 	const table = useReactTable({
 		data,
@@ -136,46 +147,35 @@ export const CustomerOrderTable = () => {
 		}
 	})
 
-	const PaginationButton = () => {
-		return (
-			<div className="flex justify-end space-x-2">
-				<Button
-					variant={"outline"}
-					onClick={() => table.previousPage()}
-					disabled={!table.getCanPreviousPage()}
-					className="hover:bg-primary-foreground hover:text-primary"
-				>
-					Previous
-				</Button>
-				<Button onClick={() => {console.log("clickkkkk"); table.nextPage();}} disabled={!table.getCanNextPage()}>
-					Next
-				</Button>
-			</div>
-		)
-	}
+	useEffect(() => {
+		table.setPageSize(5)
+	}, [])
 
 	return (
 		<div className="w-full h-full p-7 space-y-4 bg-white rounded-[32px]">
-            <div className="flex justify-end place-items-center">
+			<div className="flex justify-end place-items-center">
 				{/* <div className="pl-1 font-bold">商品名稱: </div> */}
-                <select
-                    value={filterField}
-                    onChange={handleFilterFieldChange}
-                    className="p-2 border rounded"
-                    // className="p-2 rounded-2xl border"
-                >
-                    <option value="orderId">訂單編號</option>
-                    <option value="customerId">顧客編號</option>
-                    <option value="createdAt">下單時間</option>
-                    <option value="product">購買產品</option>
-                </select>
-                <div style={{ width: "15px" }}></div>
-                <Input
-                    placeholder={`search ${filterField}...`}
-                    value={(table.getColumn(filterField)?.getFilterValue() as string) ?? ""}
-                    onChange={(event) => table.getColumn(filterField)?.setFilterValue(event.target.value)}
-                    className="w-1/3"
-                />
+				<div className="w-32">
+					<Select defaultValue="orderId" onValueChange={(value) => setFilterField(value)}>
+						<SelectTrigger>
+							<SelectValue placeholder={"Select search param key."} />
+						</SelectTrigger>
+						<SelectContent className="bg-background">
+							<SelectItem value="orderId">訂單編號</SelectItem>
+							<SelectItem value="customerId">顧客編號</SelectItem>
+							<SelectItem value="createAt">下單時間</SelectItem>
+							<SelectItem value="product">購買產品</SelectItem>
+						</SelectContent>
+					</Select>
+				</div>
+
+				<div style={{ width: "15px" }}></div>
+				<Input
+					placeholder={`search ${filterField}...`}
+					value={(table.getColumn(filterField)?.getFilterValue() as string) ?? ""}
+					onChange={(event) => table.getColumn(filterField)?.setFilterValue(event.target.value)}
+					className="w-1/3"
+				/>
 			</div>
 			<div className="rounded-2xl border">
 				<Table>
@@ -215,7 +215,7 @@ export const CustomerOrderTable = () => {
 					</TableBody>
 				</Table>
 			</div>
-			<PaginationButton />
+			<PaginationButton table={table} />
 		</div>
 	)
 }
