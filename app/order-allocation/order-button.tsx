28c9f51804ae2cc57b9bff71b722907fc0abc2aa
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-09 07:38:28 +0800

    refactor(order-button): rebuild button with dropdown menu

--- app/order-allocation/order-button.tsx
+++ app/order-allocation/order-button.tsx
@@ -2,50 +2,42 @@
 
 // components
 import { Button } from "@/components/ui/button"
-// hooks
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+// types
+import { Factory } from "@/types/factory"
 import { useState } from "react"
+// utils
+import { cn } from "@/lib/utils"
 
 type Props = {
-	onSelect?: (factory: string) => void
+	orderId: string
+	factories: Factory[]
 }
 
-const OrderButton = ({ onSelect }: Props) => {
-	const [showDropdown, setShowDropdown] = useState(false)
-
-	const handleFactorySelect = (factory: string) => {
-		setShowDropdown(false)
-		if (onSelect) {
-			onSelect(factory)
-		}
-	}
+export const OrderButton = ({ orderId, factories }: Props) => {
+	const [alloted, setAlloted] = useState(false)
 
 	return (
-		<div className="relative inline-block">
-			<Button
-				variant={"outline"}
-				onClick={() => setShowDropdown(true)}
-				className="mt-1 px-2 py-1 rounded transition-all"
-			>
-				分配訂單
-			</Button>
-			{showDropdown && (
-				<div className="absolute top-full left-0 mt-1 bg-white border border-gray-200 rounded-md overflow-hidden">
-					<ul>
-						<li onClick={() => handleFactorySelect("工廠十")}>工廠十</li>
-						<li onClick={() => handleFactorySelect("工廠三")}>工廠三</li>
-						<li onClick={() => handleFactorySelect("工廠七")}>工廠七</li>
-						<li onClick={() => handleFactorySelect("工廠四")}>工廠四</li>
-						<li onClick={() => handleFactorySelect("工廠八")}>工廠八</li>
-						<li onClick={() => handleFactorySelect("工廠二")}>工廠二</li>
-						<li onClick={() => handleFactorySelect("工廠一")}>工廠一</li>
-						<li onClick={() => handleFactorySelect("工廠六")}>工廠六</li>
-						<li onClick={() => handleFactorySelect("工廠九")}>工廠九</li>
-						<li onClick={() => handleFactorySelect("工廠五")}>工廠五</li>
-					</ul>
-				</div>
-			)}
-		</div>
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant={"secondary"} className={cn(alloted ? "hidden" : "", "bg-base text-foreground hover:text-background")}>
+					分配訂單
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="bg-background rounded-lg">
+				{factories.map((factory) => (
+          <DropdownMenuItem key={factory.id} onClick={async () => {
+            setAlloted(true)
+          }} className="text-foreground">
+						{factory.name}
+					</DropdownMenuItem>
+				))}
+			</DropdownMenuContent>
+		</DropdownMenu>
 	)
 }
-
-export default OrderButton
