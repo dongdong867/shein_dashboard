0ab95c12a88239f8143c97a26b450eee70fe39da
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-01-09 04:45:44 +0800

    refactor(factory-detail): rebuild factory detail with tailwind

--- app/order-query/components/factory-detail.tsx
+++ app/order-query/components/factory-detail.tsx
@@ -5,155 +5,85 @@ import { Factory } from "@/types/factory"
 import React from "react"
 // icons
 import { FaPhone, FaMapMarkerAlt, FaIdCard, FaClipboardList, FaCog, FaTruck } from "react-icons/fa"
+import { Badge } from "@/components/ui/badge"
+import { Separator } from "@/components/ui/separator"
+import { Progress } from "@/components/ui/progress"
 
 type FactoryDetailProps = {
 	factory: Factory
-	order: FactoryOrder[]
+	orders: FactoryOrder[]
 }
 
-export const FactoryDetail = ({ factory, order }: FactoryDetailProps) => {
-	const statusClassName = factory.status === "working" ? "bg-blue-400" : "bg-gray-400"
+const StatusIcons = ({ status, percentage }: { status: "inline" | "processing" | "shipping", percentage: number }) => {
+	return (
+		<div className="w-full flex justify-between place-items-center p-2">
+			<FaClipboardList className={status == "inline" ? "text-blue-700/80" : "text-base-100/60"} />
+			<Separator className="w-5/12 h-0.5" />
+			<FaCog className={status == "processing" ? "text-blue-700/80" : "text-base-100/60"} />
+			{status == "processing" && <div className="text-blue-700/80">({percentage}%)</div>}
+			<Separator className="w-5/12 h-0.5" />
+			<FaTruck className={status == "shipping" ? "text-blue-700/80" : "text-base-100/60"} />
+		</div>
+	)
+}
 
-	const factoryOrder = () => {
+export const FactoryDetail = ({ factory, orders }: FactoryDetailProps) => {
+	const statusClassName = factory.status === "working" ? "bg-blue-400" : "bg-gray-400"
+	
+
+	const FactoryOrderStatus = () => {
 		return (
-			<div className="text-black">
-				<ul>
-					{factory.schedule.map((factoryOrder) => (
-						<div key={factoryOrder.orderId} className="order-wrapper">
-							<div
-								style={{
-									position: "relative",
-									height: "1px",
-									border: "1.3px solid rgba(0, 0, 0, 0.06)",
-									width: "91%",
-									margin: "0 auto",
-									zIndex: 1,
-									top: "51.5px"
-								}}
-							/>
-							<li
-								key={factoryOrder.orderId}
-								style={{ marginTop: "40px", position: "relative", paddingTop: "20px" }}
-							>
-								{order
-									.filter((o) => o.orderId === factoryOrder.orderId)
-									.map((correspondingOrder) => (
-										<React.Fragment key={correspondingOrder.orderId}>
-											<div style={{ position: "absolute", top: "-25px", fontSize: "1.05em" }}>
-												{correspondingOrder.orderId}
-											</div>
-											<div
-												style={{
-													position: "absolute",
-													width: "100%",
-													height: "17px",
-													backgroundColor: "rgba(0, 0, 0, 0.1)",
-													zIndex: 1,
-													borderRadius: "10px"
-												}}
-											>
-												<div
-													style={{
-														position: "absolute",
-														top: "-17px",
-														left: "50%",
-														transform: "translateX(-50%)",
-														display: "flex",
-														justifyContent: "space-between",
-														width: "100%"
-													}}
-												>
-													<FaClipboardList
-														style={{
-															fontSize: "15px",
-															color:
-																correspondingOrder.status === "inline"
-																	? "rgba(0, 0, 255, 0.5)"
-																	: "rgba(0, 0, 0, 0.2)",
-															zIndex: 3
-														}}
-													/>
-													<FaCog
-														style={{
-															fontSize: "15px",
-															color:
-																correspondingOrder.status === "processing"
-																	? "rgba(0, 0, 255, 0.5)"
-																	: "rgba(0, 0, 0, 0.2)",
-															zIndex: 3
-														}}
-													/>
-													<FaTruck
-														style={{
-															fontSize: "15px",
-															color:
-																correspondingOrder.status === "shipping"
-																	? "rgba(0, 0, 255, 0.5)"
-																	: "rgba(0, 0, 0, 0.2)",
-															zIndex: 3
-														}}
-													/>
-												</div>
-											</div>
-											<div
-												style={{
-													position: "absolute",
-													width: `${correspondingOrder.schedulePercentage}%`,
-													height: "17px",
-													backgroundColor: "rgba(0, 0, 255, 0.45)",
-													zIndex: 3,
-													borderRadius: "10px"
-												}}
-											></div>
-											<div
-												className="text-center"
-												style={{ position: "relative", bottom: "3.7px", zIndex: 3 }}
-											>
-												{correspondingOrder.schedulePercentage}%
-											</div>
-										</React.Fragment>
-									))}
-							</li>
-						</div>
-					))}
-				</ul>
-			</div>
+			<>
+				{factory.schedule.map((factoryOrder) => (
+					<div key={factoryOrder.orderId}>
+						{orders
+							.filter((order) => order.orderId === factoryOrder.orderId)
+							.map((order) => (
+								<div key={order.orderId}>
+									<div>Order ID: {order.orderId}</div>
+									<StatusIcons status={order.status} percentage={order.schedulePercentage} />
+									<Progress value={order.schedulePercentage} className="bg-base *:bg-background" />
+								</div>
+							))}
+					</div>
+				))}
+			</>
 		)
 	}
 
 	return (
-		<div className="w-full h-full p-5 space-y-1 bg-white rounded-[20px]">
+		<div className="w-full h-full p-5 space-y-1 bg-white rounded-3xl">
 			<div className="flex justify-between items-center">
-				<div className="font-bold" style={{ fontSize: "1.3em" }}>
-					{factory.name}
-				</div>
-				<div
-					className={`flex items-center font-bold rounded-[20px] py-0.8 px-1 ${statusClassName} text-white`}
-				>
+				<div className="text-xl font-bold">{factory.name}</div>
+				<Badge variant={"outline"} className={`${statusClassName} text-white text-sm py-1`}>
 					{factory.status}
+				</Badge>
+			</div>
+
+			<div className="flex flex-col text-base-100/60 pt-2">
+				<div className="flex place-items-center text-sm">
+					<FaIdCard className="mr-1" />
+					{factory.id}
+				</div>
+				<div className="flex place-items-center text-sm">
+					<FaPhone className="mr-1" />
+					{factory.phone}
+				</div>
+				<div className="flex place-items-center text-sm">
+					<FaMapMarkerAlt className="mr-1" />
+					{factory.address}
 				</div>
 			</div>
-			<div className="flex flex-col text-gray-400">
-				<div className="flex items-center">
-					<FaIdCard className="mr-1" style={{ fontSize: "0.7em" }} />
-					<div style={{ fontSize: "0.9em" }}>{factory.id}</div> <span className="mr-5">&nbsp;</span>
-					<FaPhone className="mr-1" style={{ fontSize: "0.68em" }} />
-					<div style={{ fontSize: "0.9em" }}>{factory.phone}</div>
-				</div>
-				<div className="flex items-center">
-					<FaMapMarkerAlt className="mr-1" style={{ fontSize: "0.8em" }} />
-					<div style={{ fontSize: "0.9em" }}>{factory.address}</div>
-				</div>
-				<hr style={{ height: "1px", border: "1.3px solid lightgrey", width: "65%" }} />
-				{factory.status === "idle" && (
-					<span className="text-black text-center" style={{ fontSize: "1.3em", marginTop: "20px" }}>
-						目前無訂單
-					</span>
-				)}
+
+			<Separator className="h-0.5 mt-1" />
+
+			{factory.status === "idle" ? (
+				<div className="text-black text-center text-xl pt-2">目前無訂單</div>
+			) : (
 				<div className="text-black">
-					<ul>{factoryOrder()}</ul>
+					<FactoryOrderStatus />
 				</div>
-			</div>
+			)}
 		</div>
 	)
 }
